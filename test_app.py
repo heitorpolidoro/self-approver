90a00f3922427a7ad9d5f1882f8e601c6bb85c37
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-28 14:47:34 -0300

    fix test

--- test_app.py
+++ test_app.py
@@ -48,82 +48,6 @@ def event(repository, issue):
     return event
 
 
-# def test_create_pr(event):
-#     """
-#     This test case tests the create_branch_handler function when there are commits between the new branch and the
-#     default branch. It checks that the function creates a pull request with the correct parameters.
-#
-#     Expected behavior:
-#     - The function should create a pull request with the title "feature".
-#     - The pull request body should include a link to the issue with the title "feature" and the body "feature body".
-#     - The pull request body should include the text "Closes #42".
-#     - The pull request should not be a draft.
-#
-#     """
-#     expected_body = """### [feature](https://github.com/heitorpolidoro/pull-request-generator/issues/42)
-#
-# feature body
-#
-# Closes #42
-#
-# """
-#     create_branch_handler(event)
-#     event.repository.create_pull.assert_called_once_with(
-#         "master",
-#         "issue-42",
-#         title="feature",
-#         body=expected_body,
-#         draft=False,
-#     )
-#     event.repository.create_pull.return_value.enable_automerge.assert_called_once_with(
-#         merge_method="SQUASH"
-#     )
-#
-#
-# def test_create_pr_no_commits(event):
-#     """
-#     This test case tests the create_branch_handler function when there are no commits between the new branch and the
-#     default branch. It checks that the function handles this situation correctly by not creating a pull request.
-#     """
-#     event.repository.create_pull.side_effect = GithubException(
-#         422, message="No commits between 'master' and 'issue-42'"
-#     )
-#     create_branch_handler(event)
-#
-#
-# def test_create_pr_other_exceptions(event):
-#     """
-#     This test case tests the create_branch_handler function when an exception other than 'No commits between master and
-#     feature' is raised. It checks that the function raises the exception as expected.
-#
-#     Expected behavior:
-#     - The function should raise a GithubException with the message "Other exception".
-#
-#     """
-#     event.repository.create_pull.side_effect = GithubException(
-#         422, message="Other exception"
-#     )
-#     with pytest.raises(GithubException):
-#         create_branch_handler(event)
-#
-#
-# def test_enable_just_automerge_on_existing_pr(event):
-#     """
-#     This test case tests the create_branch_handler function when a pull request already exists for the new branch.
-#     It checks that the function enables auto-merge for the existing pull request and does not create a new pull request.
-#
-#     Expected behavior:
-#     - The function should not create a new pull request.
-#     - The function should enable auto-merge for the existing pull request with the merge method "SQUASH".
-#
-#     """
-#     existing_pr = Mock()
-#     event.repository.get_pulls.return_value = [existing_pr]
-#     create_branch_handler(event)
-#     event.repository.create_pull.assert_not_called()
-#     existing_pr.enable_automerge.assert_called_once_with(merge_method="SQUASH")
-
-
 class TestApp(TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -140,7 +64,7 @@ class TestApp(TestCase):
         """
         response = self.app.get("/")
         assert response.status_code == 200
-        assert response.text == "Pull Request Generator App up and running!"
+        assert response.text == "Self Approver App up and running!"
 
     def test_webhook(self):
         """
